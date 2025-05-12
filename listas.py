3daac2f627b2ee2b0dcfd8aceb29e8854d8a235c
juliamf791 <julia.mf8@puccampinas.edu.br>
2025-05-12 09:22:40 -0300

    criação listas e dicio
    CRIAÇÃO LISTAS E DICIO

---
+++ listas.py
@@ -0,0 +1,9 @@
+#dicionarios voos-numero do voo 'chave' cidade,cidadeorigem,cidadedestino,numerodeescalas,precodapassagem,quantidadedelugaresdisp
+#dicionarios passageiros- cpf 'chave' nome,telefone,passagenscompradas
+lista_voos_disponivel=[]
+#essas listas vao ficar dentro de voos disponiveis
+lista_codigovoo=[]
+lista_cidadeorigem=[]
+lista_cidadedestino=[]
+lista_abordo=[]#guarda o cpf,passagens e numero do voo
+lista_passageiros=[]
